70719fb905bd72307fdcc59a7322f9eb7be65644
Ahmad-A-AlTamim <ahmadabedtamim@gmail.com>
2025-01-16 23:31:24 +0200

    kos om baker

--- site/index.php
+++ site/index.php
@@ -3,7 +3,7 @@
     $username = "root";
     $password = "123";
     $conn = mysqli_connect($servername, $username, $password);
-
+    echo "<h1>Fuck Baker</h1>";
     if (!$conn) {
         die("Connection failed: " . mysqli_connect_error());
         echo "Connection failed";
