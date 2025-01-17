a9abef9152379607da1efd474fcc290a5fce547d
Ahmad-A-AlTamim <ahmadabedtamim@gmail.com>
2025-01-17 14:55:47 +0200

    v5

--- site/connect.php
+++ site/connect.php
@@ -1,22 +1,29 @@
 <?php
-    $servername = "db";
-    $username = "root";
-    $password = "123";
-    $conn = mysqli_connect($servername, $username, $password);
+$servername = "db";
+$username = "root";
+$password = "123";
+$dbname = "mydb";
 
-    if (!$conn) {
-        die("Connection failed: " . mysqli_connect_error());
-    }
-    echo "<form method='post'>";
-    echo "<input type='text' value='enter your name' name='name'>";
-    echo "<input type='submit' value='submit'>";
-    echo "</form>";
-    $name = $_POST['name'];
-    echo $name;
-    $sql = "INSERT INTO mydb.dect  VALUES ('$name')";
-    if (mysqli_query($conn, $sql)) {
-        echo "New record created successfully";
+$conn = new mysqli($servername, $username, $password, $dbname);
+
+// Check connection
+if ($conn->connect_error) {
+    die("Connection failed: " . $conn->connect_error);
+}
+
+if ($_SERVER["REQUEST_METHOD"] == "POST") {
+    $input_word = $conn->real_escape_string($_POST['input_word']);
+    $sql = "SELECT machine_word AS matched_word FROM words WHERE input_word = '$input_word'";
+    $result = $conn->query($sql);
+
+    if ($result->num_rows > 0) {
+        $row = $result->fetch_assoc();
+        $matched_word = $row['matched_word'];
+        echo json_encode(['success' => true, 'matched_word' => $matched_word]);
     } else {
-        echo "Error: " . $sql . "<br>" . mysqli_error($conn);
+        echo json_encode(['success' => false, 'message' => 'Word not found']);
     }
-    ?>
+}
+
+$conn->close();
+?>
